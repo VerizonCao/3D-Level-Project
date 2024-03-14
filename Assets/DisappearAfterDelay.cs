960cc1f2f53f3f7174b9f330ef7fbf847c6c93a5
tlx1183637121 <ltian5@ucsc.edu>
2024-03-13 19:58:49 -0700

    UI polish & puzzle

---
+++ Assets/DisappearAfterDelay.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class DisappearAfterDelay : MonoBehaviour
+{
+    public float delayInSeconds = 3f; // 设置延迟时间，默认为3秒
+
+    private void OnEnable()
+    {
+        // 在物体被激活时开始计时
+        Invoke("HideObject", delayInSeconds);
+    }
+
+    private void HideObject()
+    {
+        // 当计时结束时将物体设置为非激活状态
+        gameObject.SetActive(false);
+    }
+}
